58daa3d35c4976ca9a5d1115ba2489dc1e18a940
Elena <vodopyan.elena@gmail.com>
2022-09-23 11:32:04 +0300

    Example_1

---
+++ Example_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите числа (q - завершить ввод): ");
+string input = Console.ReadLine();
+int count = 0;
+while (input != "q")
+{
+    int number = Convert.ToInt32(input);
+    if (number > 0)
+    {
+        count += 1;
+    }
+    input = Console.ReadLine();
+}
+Console.WriteLine($"Количество чисел больше 0 = {count}");
